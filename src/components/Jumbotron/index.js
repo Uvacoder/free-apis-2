69e72ab19e439b257414ac3e8947454571fba649
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2021-11-22 22:02:35 -0500

    Update index.js

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -12,7 +12,7 @@ function Jumbotron() {
 					className='text-6xl xs:text-7xl sm:text-8xl font-bold
                     text-center mb-6 mt-12'
 				>
-					Free APIs
+					Free API Directory
 				</div>
 				<Subtitle />
 				<ButtonRows />
